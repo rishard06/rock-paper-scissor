51386b456c357218a70a1ecc106ac40dc74efb11
Richard <123456789+macabusrichard@gmail.com>
2024-04-23 13:05:24 +0800

    Add a new function functiion: write a logic that play single round and print a who wins User or Computer.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-console.log("Hello, World");
+// console.log("Hello, World");
 
 //rite the logic to get the computer choice 
 function getComputerChoice(choice) {
+  let random1 = Math.floor(Math.random() * 3) + 1;
+  choice = random1;
+  
   if (choice === 1){
     return "Rock";
   }else if (choice === 2){
@@ -11,16 +14,40 @@ function getComputerChoice(choice) {
   }
 }
 
-function randomNumber() {
-  let random1 = Math.floor(Math.random() * 3) + 1;
-  return random1
-}
-let random = randomNumber();
-console.log("Computer answer: " + getComputerChoice(random));
-
 // Write the logic to get human choice
 function getHumanChoice() {
-  choice = prompt("Choose one Rock, Paper, or Scissor? ");
+  let choice = prompt("Choose one Rock, Paper, or Scissor? ");
   return choice;
 }
-console.log("Your answer: " + getHumanChoice());
+let itsHumanChoice = getHumanChoice();
+let itsComputerChoice = getComputerChoice();
+
+console.log("Your answer: " + itsHumanChoice);
+console.log("Computer answer: " + itsComputerChoice);
+//alert("Computer answer: " + getComputerChoice());
+
+//Declare the player score variable
+const humanScore = 0;
+const computerScore = 0;
+
+//Write the logic to play single round
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "Rock" && computerChoice === "Paper") {
+    return "Coputer Win!";
+  }else if (humanChoice === "Paper" && computerChoice === "Scissor") {
+    return "Computer win!";
+  }else if (humanChoice === "Scissor" && computerChoice === "Rock") {
+    return "Computer Win!"
+  }if (humanChoice === "Rock" && computerChoice === "Scissor") {
+    return "You win!";
+  }if (humanChoice === "Paper" && computerChoice === "Rock") {
+    return "You win!";
+  }if (humanChoice === "Scissor" && computerChoice === "Paper") {
+    return "You win!";
+  }else if (humanChoice === computerChoice) {
+    return "Its a Draw!";
+  }
+}
+//const humanSelection = getHumanChoice();
+//const computerSelection =  getComputerChoice(random);
+console.log(playRound(itsHumanChoice, itsComputerChoice));
