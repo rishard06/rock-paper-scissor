7921c108ccac0d93629e7b03cab070ee78cb50cb
Richard <123456789+macabusrichard@gmail.com>
2024-04-23 20:26:17 +0800

    Update the playround function modified message when the user win/lose increment the score of user and coputer if they win

--- script.js
+++ script.js
@@ -6,48 +6,61 @@ function getComputerChoice(choice) {
   choice = random1;
   
   if (choice === 1){
-    return "Rock";
+    return "rock";
   }else if (choice === 2){
-    return "Paper";
+    return "paper";
   }else if (choice === 3){
-    return "Scissor";
+    return "scissor";
   }
 }
 
 // Write the logic to get human choice
 function getHumanChoice() {
   let choice = prompt("Choose one Rock, Paper, or Scissor? ");
+  choice = choice.toLowerCase();
   return choice;
 }
 let itsHumanChoice = getHumanChoice();
 let itsComputerChoice = getComputerChoice();
 
-console.log("Your answer: " + itsHumanChoice);
-console.log("Computer answer: " + itsComputerChoice);
+console.log("Your answer: " + itsHumanChoice.charAt(0).toUpperCase() + itsHumanChoice.slice(1));
+console.log("Computer answer: " + itsComputerChoice.charAt(0).toUpperCase() + itsComputerChoice.slice(1));
 //alert("Computer answer: " + getComputerChoice());
 
 //Declare the player score variable
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 //Write the logic to play single round
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice === "Rock" && computerChoice === "Paper") {
-    return "Coputer Win!";
-  }else if (humanChoice === "Paper" && computerChoice === "Scissor") {
-    return "Computer win!";
-  }else if (humanChoice === "Scissor" && computerChoice === "Rock") {
-    return "Computer Win!"
-  }if (humanChoice === "Rock" && computerChoice === "Scissor") {
-    return "You win!";
-  }if (humanChoice === "Paper" && computerChoice === "Rock") {
-    return "You win!";
-  }if (humanChoice === "Scissor" && computerChoice === "Paper") {
-    return "You win!";
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++
+    return "You Lose! Paper beats Rock";
+  }else if (humanChoice === "paper" && computerChoice === "scissor") {
+    computerScore++
+    return "You Lose! Scissor beat Paper";
+  }else if (humanChoice === "scissor" && computerChoice === "Rock") {
+    computerScore++
+    return "You Lose! Rock beats Scissor";
+  }if (humanChoice === "rock" && computerChoice === "scissor") {
+    humanScore++
+    return "You win! Rock beats Scissor";
+  }if (humanChoice === "paper" && computerChoice === "sock") {
+    humanScore++
+    return "You win! Paper beats Rock";
+  }if (humanChoice === "scissor" && computerChoice === "paper") {
+    humanScore++
+    return "You win! Scissor beat Paper" ;
   }else if (humanChoice === computerChoice) {
     return "Its a Draw!";
   }
 }
-//const humanSelection = getHumanChoice();
-//const computerSelection =  getComputerChoice(random);
 console.log(playRound(itsHumanChoice, itsComputerChoice));
+console.log("Computer Score: " + computerScore);
+console.log("Your Score: " + humanScore);
+
+// Write the logic to play the entire game
+
+function playGame() {
+  
+}
